058688dbe2e1c9b105495fb872318ffd6790418e
renzaspiras <aspirasrenz@gmail.com>
2024-03-30 09:41:24 +0800

    documentation patch

--- cm.sh
+++ cm.sh
@@ -1,3 +1,5 @@
+# This file is intended for Arch-Termux OS only; do not run it on a different OS.
+
 # compile
 gcc donut.c -o donut -lm
 
--- cmrm.sh
+++ cmrm.sh
@@ -1,2 +1,4 @@
+# This file is intended for Arch-Termux OS only; do not run it on a different OS.
+
 rm /bin/donut
 rm -rf ~/apps/donut
